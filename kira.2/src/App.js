30bdccc09486a48a170325519d8a362dd30e268d
ahmedaminahmed_amin <ahmedamineashad@gmail.com>
2023-04-04 09:44:06 +0200

    first commit

---
+++ kira.2/src/App.js
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <>
+<header>
+          <a href="https://www.youtube.com/c/ElzeroInfo" target="_blank" title="ELZERO" className="css"> CSS </a>
+        </header>
+        <section>
+          <form className="box">
+            <h1>LOGIN</h1>
+            <div className="smallbox">
+              <input type="text" placeholder="Username" required />
+              <input type="password" placeholder="password" />
+            </div>
+            <title>submit</title>
+            <input id="value" type="submit" />
+          </form>
+        </section>
+    </>
+  );
+}
+
+export default App;
